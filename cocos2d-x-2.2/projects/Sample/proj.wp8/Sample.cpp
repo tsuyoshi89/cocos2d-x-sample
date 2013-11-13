8ad0d8d6d9f65b413e6aaf2e9085da5fa38541f8
tsuyoshi89 <tsuyoshi@miyano-harikyu.jp>
2013-11-13 08:24:03 -0800

    create sample project

---
+++ cocos2d-x-2.2/projects/Sample/proj.wp8/Sample.cpp
@@ -0,0 +1,149 @@
+﻿#include <wrl/client.h>
+#include <d3d11_1.h>
+#include <DirectXMath.h>
+#include <memory>
+#include <agile.h>
+#include <ppltasks.h>
+#include "Sample.h"
+#include "CCApplication.h"
+
+using namespace Windows::ApplicationModel;
+using namespace Windows::ApplicationModel::Core;
+using namespace Windows::ApplicationModel::Activation;
+using namespace Windows::UI::Core;
+using namespace Windows::System;
+using namespace Windows::Foundation;
+using namespace Windows::Graphics::Display;
+using namespace Windows::Phone::UI::Input;
+using namespace Windows::Graphics::Display;
+using namespace concurrency;
+USING_NS_CC;
+
+Sample::Sample()
+{
+}
+
+void Sample::Initialize(CoreApplicationView^ applicationView)
+{
+	applicationView->Activated +=
+		ref new TypedEventHandler<CoreApplicationView^, IActivatedEventArgs^>(this, &Sample::OnActivated);
+
+	CoreApplication::Suspending +=
+		ref new EventHandler<SuspendingEventArgs^>(this, &Sample::OnSuspending);
+
+	CoreApplication::Resuming +=
+		ref new EventHandler<Platform::Object^>(this, &Sample::OnResuming);
+}
+
+void Sample::SetWindow(CoreWindow^ window)
+{
+    // Specify the orientation of your application here
+    // The choices are DisplayOrientations::Portrait or DisplayOrientations::Landscape or DisplayOrientations::LandscapeFlipped
+	DisplayProperties::AutoRotationPreferences = DisplayOrientations::Landscape;
+
+	window->VisibilityChanged +=
+		ref new TypedEventHandler<CoreWindow^, VisibilityChangedEventArgs^>(this, &Sample::OnVisibilityChanged);
+
+	window->Closed += 
+		ref new TypedEventHandler<CoreWindow^, CoreWindowEventArgs^>(this, &Sample::OnWindowClosed);
+
+	window->PointerPressed +=
+		ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &Sample::OnPointerPressed);
+
+	window->PointerMoved +=
+		ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &Sample::OnPointerMoved);
+
+	window->PointerReleased +=
+		ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &Sample::OnPointerReleased);
+
+    CCEGLView* eglView = new CCEGLView();
+	eglView->Create(window);
+    eglView->setViewName("Sample");
+}
+
+void Sample::Load(Platform::String^ entryPoint)
+{
+}
+
+void Sample::Run()
+{
+    CCApplication::sharedApplication()->run();
+}
+
+void Sample::Uninitialize()
+{
+}
+
+void Sample::OnVisibilityChanged(CoreWindow^ sender, VisibilityChangedEventArgs^ args)
+{
+	CCEGLView::sharedOpenGLView()->OnVisibilityChanged(sender, args);
+}
+
+void Sample::OnWindowClosed(CoreWindow^ sender, CoreWindowEventArgs^ args)
+{
+	CCEGLView::sharedOpenGLView()->OnWindowClosed(sender, args);
+}
+
+void Sample::OnPointerPressed(CoreWindow^ sender, PointerEventArgs^ args)
+{
+	CCEGLView::sharedOpenGLView()->OnPointerPressed(sender, args);
+}
+
+void Sample::OnPointerMoved(CoreWindow^ sender, PointerEventArgs^ args)
+{
+	CCEGLView::sharedOpenGLView()->OnPointerMoved(sender, args);
+}
+
+void Sample::OnPointerReleased(CoreWindow^ sender, PointerEventArgs^ args)
+{
+	CCEGLView::sharedOpenGLView()->OnPointerReleased(sender, args);
+}
+
+void Sample::OnActivated(CoreApplicationView^ applicationView, IActivatedEventArgs^ args)
+{
+	HardwareButtons::BackPressed += ref new EventHandler<BackPressedEventArgs^>(this, &Sample::OnBackButtonPressed);   
+	CoreWindow::GetForCurrentThread()->Activate();
+}
+
+void Sample::OnBackButtonPressed(Object^ sender, BackPressedEventArgs^ args)
+{
+    // Leave args->Handled set to false and the app will quit when user presses the back button on the phone
+}
+
+void Sample::OnSuspending(Platform::Object^ sender, SuspendingEventArgs^ args)
+{
+	// Save app state asynchronously after requesting a deferral. Holding a deferral
+	// indicates that the application is busy performing suspending operations. Be
+	// aware that a deferral may not be held indefinitely. After about five seconds,
+	// the app will be forced to exit.
+	SuspendingDeferral^ deferral = args->SuspendingOperation->GetDeferral();
+	//m_renderer->ReleaseResourcesForSuspending();
+
+	create_task([this, deferral]()
+	{
+		// Insert your code here.
+
+		deferral->Complete();
+	});
+}
+ 
+void Sample::OnResuming(Platform::Object^ sender, Platform::Object^ args)
+{
+	// Restore any data or state that was unloaded on suspend. By default, data
+	// and state are persisted when resuming from suspend. Note that this event
+	// does not occur if the app was previously terminated.
+	// m_renderer->CreateWindowSizeDependentResources();
+}
+
+IFrameworkView^ Direct3DApplicationSource::CreateView()
+{
+	return ref new Sample();
+}
+
+[Platform::MTAThread]
+int main(Platform::Array<Platform::String^>^)
+{
+	auto direct3DApplicationSource = ref new Direct3DApplicationSource();
+	CoreApplication::Run(direct3DApplicationSource);
+	return 0;
+}
